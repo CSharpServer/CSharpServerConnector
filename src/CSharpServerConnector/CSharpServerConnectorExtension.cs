be0548f1cecfd8d1d5197feb151ad49721bbb1b2
AlexChow <cplover@live.com>
2015-12-12 20:56:26 +0800

    init commit

---
+++ src/CSharpServerConnector/CSharpServerConnectorExtension.cs
@@ -0,0 +1,108 @@
+﻿using CSharpServerFramework.Extension;
+using CSharpServerFramework;
+using CSharpServerFramework.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSServerJsonProtocol;
+using Newtonsoft.Json;
+
+namespace CSharpServerConnector
+{
+    public class CSharpServerConnectorManager
+    {
+        internal static CSharpServerConnectorManager _instance;
+        internal static CSharpServerConnectorManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    throw new Exception("Not Use CSharpServerConnectorExtension");
+                }
+                return _instance;
+            }
+        }
+
+        public CSharpServerConnectorManager()
+        {
+            Connectors = new Dictionary<object, CSharpServerConnector>();
+        }
+
+        public IDictionary<object, CSharpServerConnector> Connectors { get; private set; }
+
+    }
+
+    public class CSharpServerConnector : ICSharpServerUser
+    {
+        public bool IsUserValidate { get { return true; } }
+
+        public ICSharpServerSession Session { get; set; }
+    }
+
+    [ValidateExtension]
+    [ExtensionInfo("CSSConnectorValidate")]
+    public class CSharpServerConnectorValidateExtension : JsonExtensionBase
+    {
+
+        public override void Init()
+        {
+            
+        }
+    }
+
+    [ExtensionInfo("CSharpServerConnector")]
+    public class CSharpServerConnectorExtension : JsonExtensionBase
+    {
+
+        public override void Init()
+        {
+            CSharpServerConnectorManager._instance = new CSharpServerConnectorManager();
+        }
+
+        [CommandInfo(1, "AcceptMsg")]
+        public void AcceptRemoteRedirectMessage(ICSharpServerSession session, dynamic msg)
+        {
+            Log("Accept Remote Redirect Message");
+            string extName = msg.ExtName;
+            string CmdName = msg.CmdName;
+            var valueObject = JsonConvert.DeserializeObject(msg.Value);
+            if (string.IsNullOrWhiteSpace(CmdName))
+            {
+                int CmdId = msg.CmdId;
+                RedirectMessage(extName, CmdId, session, valueObject);
+            }
+            else
+            {
+                RedirectMessage(extName, CmdName, session, valueObject);
+            }
+        }
+    }
+
+    public static class CSharpServerBuilderExtension
+    {
+        public static CSharpServerConnectorManager UseConnectorExtension(this ICSServerBuilder Builder)
+        {
+            var extension = new CSharpServerConnectorExtension();
+            Builder.UseExtension(extension);
+            return CSharpServerConnectorManager.Instance;
+        }
+    }
+
+    public static class ConnectorRedirectExtension
+    {
+        public static void RedirectMessageToRemote(this ICSharpServerExtension Extension, ICSharpServerSession UserSession, object RemoteId, dynamic Value, string RemoteExtName, string RemoteCmdName, int RemoteCmdId = -1)
+        {
+            var aptMsg = new
+            {
+                Value = JsonConvert.SerializeObject(Value),
+                ExtName = RemoteExtName,
+                CmdId = RemoteCmdId,
+                CmdName = RemoteCmdName
+            };
+            var connector = CSharpServerConnectorManager.Instance.Connectors[RemoteId];
+            Extension.SendJsonResponse(connector.Session, aptMsg, "CSharpServerConnector", "AcceptMsg");
+        }
+    }
+}
---
+++ src/CSharpServerConnector/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSharpServerConnector")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpServerConnector")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e19c6fbb-8c49-4b4f-a21b-fcff5527fc1a")]
